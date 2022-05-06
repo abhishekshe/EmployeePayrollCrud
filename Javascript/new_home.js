d3fee11f339cfcef2632129778bb0138c7f0cc17
Abhishek Narwariya <abhisheknarwariya813@gmail.com>
2022-05-06 15:09:39 +0530

    Adding UC3

--- Javascript/new_home.js
+++ Javascript/new_home.js
@@ -6,9 +6,9 @@ window.addEventListener('DOMContentLoaded',(event)=>
     empPayrollList= getEmployeePayrollDataFromStorage();
     
     document.querySelector(".emp-count").textContent= empPayrollList.length;
-  
+    
     createInnerHtml();
-   
+    
     localStorage.removeItem('editEmp');
 });
 
@@ -20,13 +20,13 @@ const createInnerHtml=()=>
 {
    
     if(empPayrollList.length==0) return;
-    
+
     const headerHtml= "<tr><th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th></tr>"
-  
+    
     let innerHtml= `${headerHtml}`;
-  
+   
     for(const empPayrollData of empPayrollList){
-      
+     
         innerHtml= `${innerHtml}
         <tr>
             <td><img class="profile" alt="" src="${empPayrollData._profilePic}"></td>
@@ -36,13 +36,14 @@ const createInnerHtml=()=>
             </td>
             <td>${empPayrollData._salary}</td>
             <td>${stringifyDate(empPayrollData._startDate)}</td>
-            <td><img id="${empPayrollData._id}" onclick= "remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
-            <img id="${empPayrollData._id}" onclick= "update(this)" alt="edit" src="../assets/icons/create-black-18dp.svg"></td>
+            <td><img id="${empPayrollData._id}" onclick= "remove(this)" alt="delete" src="C:\Users\abhis\OneDrive\Desktop\Day48\assets\assets\icons\delete-black-18dp.svg">
+            <img id="${empPayrollData._id}" onclick= "update(this)" alt="edit" src="C:\Users\abhis\OneDrive\Desktop\Day48\assets\assets\icons\create-black-18dp.svg"></td>
         </tr>`;
     }
-   
+    
     document.querySelector('#table-display').innerHTML=innerHtml;
 }
+
 
 const createEmployeePayrollJSON = () => {
     let empPayrollListLocal = [
@@ -93,24 +94,24 @@ const createEmployeePayrollJSON = () => {
       if(!empPayrollData) return;
       
       const index= empPayrollList.map(empData=>empData._id).indexOf(empPayrollData.id);
-    
+      
       empPayrollList.splice(index,1);
-    
+      
       localStorage.setItem("EmployeePayrollList",JSON.stringify(empPayrollList));
-      
+     
       document.querySelector(".emp-count").textContent= empPayrollList.length;
-      
+     
       createInnerHtml();
   }
 
   
   const update= (node)=>{
      
-      let empPayrollData= empPayrollList.find(empData=>empData._id= node.id);
-      
+      let empPayrollData= empPayrollList.find(empData=>empData._id== node.id);
+    
       if(!empPayrollData) return;
-      
+
       localStorage.setItem('editEmp',JSON.stringify(empPayrollData));
-     
+      
       window.location.replace(site_properties.emp_payroll_page);
   }
