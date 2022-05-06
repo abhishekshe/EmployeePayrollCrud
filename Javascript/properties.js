963abb232002d1cc5b2c5305e694a5de1a6fe8d6
Abhishek Narwariya <abhisheknarwariya813@gmail.com>
2022-05-06 14:18:19 +0530

    Adding UC1

---
+++ Javascript/properties.js
@@ -0,0 +1,5 @@
+//this js file is defined to help reducing hard coded values in app and use dry principle->do not repeat yourself..
+let site_properties={
+    home_page: "C:\Users\abhis\OneDrive\Desktop\Day48\EmployeePayroll\Page\payrollHome.html",
+    emp_payroll_page:"C:\Users\abhis\OneDrive\Desktop\Day48\EmployeePayroll\Page\Payrollform.html"
+};
